00ee8af90b2892d5177ffa3e07adb428d5c4207c
Stojce Slavkovski <>
2019-04-07 11:54:11 +0200

    initial

---
+++ README.md
@@ -0,0 +1,27 @@
+# Macedonian translation for react-admin
+
+Macedonian translation for [react-admin](https://github.com/marmelab/react-admin), the frontend framework for building admin applications on top of REST services.
+
+## Installation
+
+```sh
+npm install --save ra-language-macedonian
+```
+
+## Usage
+
+```js
+import macedonianMessages from "ra-language-macedonian";
+
+const messages = {
+  mk: macedonianMessages
+};
+
+<Admin locale="mk" messages={messages}>
+  ...
+</Admin>;
+```
+
+## License
+
+This translation is licensed under the [MIT Licence](LICENSE).
---
+++ index.js
@@ -0,0 +1,61 @@
+export default {
+  aor: {
+    action: {
+      delete: "Избриши",
+      show: "Покажи",
+      list: "Листа",
+      save: "Сними",
+      create: "Креирај",
+      edit: "Измени",
+      cancel: "Откажи",
+      refresh: "Освежи",
+      add_filter: "Додади филтер",
+      remove_filter: "Избриши филтер",
+      back: "Назад"
+    },
+    boolean: {
+      true: "Да",
+      false: "Не"
+    },
+    page: {
+      list: "Листа %{name}",
+      edit: "%{name} #%{id}",
+      show: "%{name} #%{id}",
+      create: "Креирај %{name}",
+      delete: "Избриши %{name} #%{id}",
+      dashboard: "Контролна табла",
+      not_found: "Не е најдено"
+    },
+    input: {
+      file: {
+        upload_several:
+          "Ispustite neke datoteke koje želite preuzeti ili kliknite da biste ih odabrali.",
+        upload_single:
+          "Ispustite datoteku koju želite preuzeti ili kliknite je da biste je odabrali."
+      },
+      image: {
+        upload_several:
+          "Ispustite neke slike za prijenos ili kliknite da biste ih odabrali.",
+        upload_single:
+          "Ispustite sliku koju želite prenijeti ili kliknite je da biste je odabrali."
+      }
+    },
+    message: {
+      yes: "Да",
+      no: "Не",
+      are_you_sure: "Дали сте сигурни?",
+      loading: "Страната се вчитува, Ве молиме почекајте",
+      about: "За",
+      not_found: "Погрешен URL или следите погрешна врска."
+    },
+    validation: {
+      required: "Задолжително",
+      minLength: "Мора да содржи најмалку %{min} карактери",
+      maxLength: "Мора да содржи најмногу %{max} карактери или помалку",
+      minValue: "Мора да биде намалку %{min}",
+      maxValue: "Мора да биде %{max} или помалку",
+      number: "Мора да биде бројка",
+      email: "Мора да биде валидна е-пошта"
+    }
+  }
+};
